4d563b0d8de847a9c049c7113ad12e2da5c43477
姚双成 <yaoshch@126.com>
2014-03-11 10:20:01 +0800

    1

--- init.el
+++ init.el
@@ -254,7 +254,7 @@ Replaces default behaviour of comment-dwim, when it inserts comment at the end o
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bmkp-last-as-first-bookmark-file "/root/.emacs.d/bookmarks")
+ '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
  '(session-use-package t nil (session))
  '(tabbar-separator (quote (0.5))))
 (custom-set-faces
